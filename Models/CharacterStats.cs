f81478adf564e56fa283fdf476284a1394b2a7cc
Lucas Felix <lucas.felixandrade@hotmail.com.br>
2023-10-18 11:02:05 -0300

    feat: new property

--- Models/CharacterStats.cs
+++ Models/CharacterStats.cs
@@ -10,10 +10,11 @@ namespace TibiaApi.Models
         public int Level { get; set; }
         public int MagicLevel { get; set; }
         public int FistFigthing { get; set; }
-        public int SwordFigthing { get; set; }
         public int ClubFigthing { get; set; }
+        public int SwordFigthing { get; set; }
         public int AxeFigthing { get; set; }
         public int DistanceFigthing { get; set; }
         public int Shielding { get; set; }
+        public int Fishing { get; set; }
     }
 }
