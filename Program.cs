7f49f1a711b8016c232a58f806b1115400643e00
Lucas Felix <lucas.felixandrade@hotmail.com.br>
2023-12-09 01:51:18 -0300

    feat: add database string connection

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options => 
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebServer"));
 });
 
 // Add services to the container.
